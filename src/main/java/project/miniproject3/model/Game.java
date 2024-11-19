158f3b6f8d26694946ae01af7fe25f2d7e7225ec
Th3Cl0wn3r <juansebastiansierracruz3773@gmail.com>
2024-11-19 17:22:14 -0500

    corrected matrix number storing

--- src/main/java/project/miniproject3/model/Game.java
+++ src/main/java/project/miniproject3/model/Game.java
@@ -68,9 +68,9 @@ public class Game implements Serializable {
     }
 
     public void placeShip(int row, int col, int rowSpan, int colSpan, int type){
-        for (int i = row; i < row + rowSpan; i++){
-            for (int j = col; j < col + colSpan; j++){
-                playerMatrix.setNumber(row, col, type);
+        for (int i = row; i < row + colSpan; i++){
+            for (int j = col; j < col + rowSpan; j++){
+                playerMatrix.setNumber(i, j, type);
             }
         }
     }
