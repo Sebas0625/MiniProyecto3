869bc51bd6a6bc28f40469e330ebd5b5cd2d793d
Th3Cl0wn3r <juansebastiansierracruz3773@gmail.com>
2024-11-21 22:44:47 -0500

    added credits

--- src/main/java/project/miniproject3/controller/WelcomeController.java
+++ src/main/java/project/miniproject3/controller/WelcomeController.java
@@ -2,16 +2,22 @@ package project.miniproject3.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 import project.miniproject3.model.FileHandling.PlainTextFileHandler;
 import project.miniproject3.model.Game;
@@ -263,6 +269,54 @@ public class WelcomeController {
     @FXML
     public void handleCredits() {
         startSound();
+
+        Stage creditsStage = new Stage();
+        creditsStage.setTitle("Créditos");
+
+        creditsStage.initModality(Modality.WINDOW_MODAL);
+        creditsStage.initStyle(StageStyle.UNDECORATED);
+
+        Label titleLabel = new Label("CRÉDITOS");
+        titleLabel.setStyle("-fx-text-fill: white;" +
+                "-fx-font-size: 24px;" +
+                "-fx-font-weight: bold;" +
+                "-fx-padding: 10px;");
+
+        Label creditLabel1 = new Label("Desarrollado por: Juan Sebástian Sierra, Daniel Andrade Reyes y Miguel Angel Castillo ");
+        Label creditLabel2 = new Label("Códigos: 202343656 - 202343792 - 2341975");
+        Label creditLabel4 = new Label("Materia: Programación Orientada a Eventos - 2024-2");
+
+        String labelStyle = "-fx-text-fill: white; -fx-text-alignment: center;";
+        creditLabel1.setStyle(labelStyle);
+        creditLabel2.setStyle(labelStyle);
+        creditLabel4.setStyle(labelStyle);
+
+        creditLabel1.setWrapText(true);
+        creditLabel2.setWrapText(true);
+        creditLabel4.setWrapText(true);
+
+        Button closeButton = new Button("Cerrar");
+        closeButton.setOnAction(e -> creditsStage.close());
+        closeButton.setStyle(
+                "-fx-background-color: #c900ff;" +
+                        "-fx-text-fill: white;" +
+                        "-fx-font-weight: bold;" +
+                        "-fx-font-size: 14px;" +
+                        "-fx-border-color: white;" +
+                        "-fx-border-width: 2px;" +
+                        "-fx-border-radius: 10px;" +
+                        "-fx-background-radius: 10px;" +
+                        "-fx-padding: 5px 10px;"
+        );
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(titleLabel, creditLabel1, creditLabel2, creditLabel4, closeButton);
+        layout.setStyle("-fx-background-color: rgba(0,0,0,0.4);");
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout, 350, 250);
+        creditsStage.setScene(scene);
+        creditsStage.showAndWait();
+
     }
 
     /**
